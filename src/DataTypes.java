43b11f88f00333b4386e9f19b505d4cf60f540b6
talhaefeustun <talhaefeustun@gmail.com>
2026-09-22 19:07:12 -0400

    done

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,7 +3,7 @@ public class DataTypes {
     public static long sum(int[] numbers) {
         long s = 0; // variable to accumulate the sum in!// changed to long to avoid integer overflow.
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (long x : numbers) {
             s += x;
         }
         return s;
